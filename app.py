e7c22890b6cd34000a2bdfa0f8ea957204ece204
Addison <74084822+addison-ch@users.noreply.github.com>
2022-01-14 09:05:33 -0500

    home page has been styled

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from generator import gen_sentence
 import json
 
+
 app = Flask(__name__)
 
 harryMarkov = json.load(open('data/harry_potter/chain/harry_chain.json'))
@@ -13,6 +14,26 @@ dumbMarkov = json.load(open('data/harry_potter/chain/dumbledore_chain.json'))
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/hp')
+def hp():
+    return render_template('hp.html')
+
+
+@app.route('/got')
+def got():
+    return render_template('hp.html')
+
+
+@app.route('/office')
+def office():
+    return render_template('hp.html')
+
+
+@app.route('/custom')
+def custom():
+    return render_template('hp.html')
 
 
 if __name__ == "__main__":
